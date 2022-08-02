0d64433059c14abf5378fb1f9282faff360ef772
Jin409 <sualng123@gmail.com>
2022-08-02 23:27:25 +0900

    fix: delete path

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from challenge.views import (
-    ChallengeRetrieveAPIView,
-)
 
 app_name = "challenge"
 
 urlpatterns = [
-    # path('/search',ChallengeRetrieveAPIView.as_view()),
+
 ]
