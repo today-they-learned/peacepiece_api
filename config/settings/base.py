c91dbdbb1296bfc05eb69653f1113e982d8659c1
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-21 17:50:49 +0900

    feat: 기본 request parser 클래스 목록 정의

--- config/settings/base.py
+++ config/settings/base.py
@@ -180,6 +180,12 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FileUploadParser',
+    )
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
