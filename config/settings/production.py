da779927b1c37dd572320a7a9924c756334d665d
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-25 19:51:55 +0900

    chore: production에서는 DEBUG false 추가

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,7 @@ import dj_database_url
 
 from .base import *  # noqa pylint: disable=wildcard-import, unused-wildcard-import
 
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 db_from_env = dj_database_url.config(conn_max_age=500)
