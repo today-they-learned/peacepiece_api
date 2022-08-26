75807db2a28b96f9e3552e713a7bf85e29843cb4
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-27 02:32:13 +0900

    chore: 루트페이지에서 Swagger 문서에 바로 접근하도록 함.

--- config/urls.py
+++ config/urls.py
@@ -35,6 +35,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("admin/", admin.site.urls),
     path("users/", include("user.urls")),
     path("articles/", include("article.urls")),
