bfdfbc77251e0a2462d86d2789108a53de3eaf03
Jin409 <sualng123@gmail.com>
2022-08-28 17:39:39 +0900

    style: 메서드 주석 추가

--- article/views/article_view_set.py
+++ article/views/article_view_set.py
@@ -29,21 +29,41 @@ class ArticleViewSet(ModelViewSet):
         return serializer.save(writer=self.request.user)
 
     def create(self, request, *args, **kwargs):
+        """
+        글을 게시합니다.
+        """
         return super().create(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        글의 상세 내용을 반환합니다.
+        """
         return super().retrieve(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
+        """
+        글을 수정합니다.
+        - 글의 게시자인 경우에만 가능합니다.
+        """
         return super().update(request, *args, **kwargs)
 
     def partial_update(self, request, *args, **kwargs):
+        """
+        글의 상세 내용을 반환합니다.
+        """
         return super().partial_update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
+        """
+        글을 삭제합니다.
+        - 글의 게시자인 경우에만 가능합니다.
+        """
         return super().destroy(request, *args, **kwargs)
 
     def list(self, request, *args, **kwargs):
+        """
+        글의 목록을 반환합니다.
+        """
         queryset = self.filter_queryset(self.get_queryset().filter(writer=self.request.user))
 
         page = self.paginate_queryset(queryset)
