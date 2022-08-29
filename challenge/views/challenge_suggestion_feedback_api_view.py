0eaf78f50a2d538bad5d22cd4cd7c9d31c7ccc23
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-29 21:31:42 +0900

    feat: challenge suggestion feeback 생성/삭제 API를 프론트에서 사용하기 쉽게 개선

---
+++ challenge/views/challenge_suggestion_feedback_api_view.py
@@ -0,0 +1,48 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+
+from challenge.models import ChallengeSuggestion, ChallengeSuggestionFeedback
+from challenge.serializers import ChallengeSuggestionFeedbackSerializer
+
+
+class ChallengeSuggestionFeedbackAPIView(GenericAPIView):
+    serializer_class = ChallengeSuggestionFeedbackSerializer
+
+    def get_success_headers(self, data):
+        try:
+            return {"Location": str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
+
+    def post(self, request, suggestion_id, *args, **kwargs):
+        """
+        챌린지 제안 좋아요 API
+        """
+        serializer = self.get_serializer(data=request.data)
+        get_object_or_404(ChallengeSuggestion, pk=suggestion_id)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            user=self.request.user,
+            suggestion_id=suggestion_id,
+        )
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def delete(self, request, suggestion_id, *args, **kwargs):
+        """
+        챌린지 제안 좋아요 취소 API
+        """
+        suggetsion_feedback = get_object_or_404(
+            ChallengeSuggestionFeedback,
+            suggestion_id=suggestion_id,
+            user=self.request.user,
+        )
+
+        suggetsion_feedback.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
