69102ae90a5e5d73d306743edc903448200cafdb
Jihyun-Choi <034179@naver.com>
2022-09-03 16:47:27 +0900

    fix: article_comment_serializer 이름 수정

--- article/serializers/__init__.py
+++ article/serializers/__init__.py
@@ -1,4 +1,4 @@
-from .article_comment import ArticleCommentSerializer
+from .article_comment_serializer import ArticleCommentSerializer
 from .article_list_serializer import ArticleListSerializer
 from .article_serializer import ArticleSerializer
 from .article_update_serializer import ArticleUpdateSerializer
--- article/serializers/article_comment_serializer.py
+++ article/serializers/article_comment_serializer.py
@@ -1,23 +1,12 @@
 from drf_writable_nested.serializers import WritableNestedModelSerializer
-from rest_framework import serializers
 
-from article.models import Article, ArticleComment
+from article.models import ArticleComment
 from user.serializers import UserAbstractSerializer
-
-from .article_serializer import ArticleSerializer
 
 
 class ArticleCommentSerializer(WritableNestedModelSerializer):
     """Serializer definition for ArticleComment Model."""
 
-    article_id = serializers.PrimaryKeyRelatedField(
-        source="article",
-        queryset=Article.objects.all(),
-        write_only=True,
-    )
-    article = ArticleSerializer(
-        read_only=True,
-    )
     writer = UserAbstractSerializer(read_only=True)
 
     class Meta:
@@ -27,7 +16,6 @@ class ArticleCommentSerializer(WritableNestedModelSerializer):
         fields = [
             "id",
             "article_id",
-            "article",
             "writer",
             "content",
             "created_at",
@@ -37,7 +25,6 @@ class ArticleCommentSerializer(WritableNestedModelSerializer):
         read_only_fields = [
             "id",
             "article_id",
-            "article",
             "writer",
             "created_at",
             "updated_at",
