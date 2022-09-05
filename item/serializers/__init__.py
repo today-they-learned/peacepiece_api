7970a71f0b6f0e1cc19d098bc3366c6692d8fb29
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-06 00:35:15 +0900

    chore: 사용하지 않을 serializer 삭제

--- item/serializers/__init__.py
+++ item/serializers/__init__.py
@@ -2,4 +2,3 @@ from .buyable_item_list_serializer import BuyableItemListSerializer
 from .item_condition_abstract_serializer import ItemConditionAbstractSerializer
 from .item_condition_serializer import ItemConditionSerializer
 from .item_serializer import ItemSerializer
-from .user_item_serializer import UserItemSerializer
