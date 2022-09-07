b65754415e11856190e3c9a203ea42c19d0fa6e0
Jin409 <sualng123@gmail.com>
2022-09-07 23:13:00 +0900

    feat: notification 모델 수정

--- notification/models/notification.py
+++ notification/models/notification.py
@@ -1,20 +1,51 @@
 from django.db import models
 
-from user.models import User
-
 
 class Notification(models.Model):
     """Model definition for Notification"""
 
+    NOTICE_CATEGORY_CHOICES = (
+        (0, "챌린지 인증글 댓글 알림"),
+        (1, "피스글 댓글 알림"),
+        (2, "카테고리 챌린지 알림"),
+    )
+
     user = models.ForeignKey(
-        User,
+        "user.User",
         on_delete=models.CASCADE,
         related_name="notifications",
     )
-    category = models.IntegerField()
+
+    contributor = models.ForeignKey(
+        "user.User",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    notice_category = models.IntegerField(choices=NOTICE_CATEGORY_CHOICES, default=0, verbose_name="알림 종류")
+
+    challenge = models.ForeignKey(
+        "challenge.Challenge",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    category = models.ForeignKey(
+        "challenge.Category",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    article = models.ForeignKey(
+        "article.Article",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
+
     updated_at = models.DateTimeField(
         auto_now=True,
     )
