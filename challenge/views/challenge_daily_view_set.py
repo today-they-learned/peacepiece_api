9c33da31d381f23dc6ff9ead80de390e359b2d2f
Jihyun-Choi <034179@naver.com>
2022-09-08 23:36:11 +0900

    fix: 코드 리뷰 반영

--- challenge/views/challenge_daily_view_set.py
+++ challenge/views/challenge_daily_view_set.py
@@ -1,16 +1,16 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.generics import ListAPIView
 
 from challenge.models import Challenge
 from challenge.serializers import ChallengeAbstractSerializer, ChallengeSerializer
-from config.viewsets import BaseViewSet
 
 
-class ChallengeDailyViewSet(BaseViewSet, ListModelMixin, RetrieveModelMixin):
-    queryset = Challenge.objects.daily()
+class ChallengeDailyViewSet(ListAPIView):
+    queryset = Challenge.objects.daily()[:3]
     serializer_class = ChallengeSerializer
-    filterset_fields = ["categories"]
-    search_fields = ["title"]
-    ordering_fields = ["point", "prover_cnt", "created_at", "updated_at"]
+    filterset_fields = []
+    search_fields = []
+    ordering_fields = []
+    pagination_class = None
 
     def get_serializer_class(self):
         if self.action == "list":
--- challenge/views/challenge_ended_view_set.py
+++ challenge/views/challenge_ended_view_set.py
@@ -1,16 +1,15 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.generics import ListAPIView
 
 from challenge.models import Challenge
 from challenge.serializers import ChallengeAbstractSerializer, ChallengeSerializer
-from config.viewsets import BaseViewSet
 
 
-class ChallengeEndedViewSet(BaseViewSet, ListModelMixin, RetrieveModelMixin):
+class ChallengeEndedViewSet(ListAPIView):
     queryset = Challenge.objects.ended()
     serializer_class = ChallengeSerializer
-    filterset_fields = ["categories"]
-    search_fields = ["title"]
-    ordering_fields = ["point", "prover_cnt", "created_at", "updated_at"]
+    filterset_fields = []
+    search_fields = []
+    ordering_fields = []
 
     def get_serializer_class(self):
         if self.action == "list":
