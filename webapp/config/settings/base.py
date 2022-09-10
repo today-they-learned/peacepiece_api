4bc8be8f7dda68ccc36a69448225e83ca34f4956
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-11 03:57:07 +0900

    fix: db settings

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -133,8 +133,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": env("POSTGRES_PORT"),
     }
 }
 
--- webapp/config/settings/production.py
+++ webapp/config/settings/production.py
@@ -1,9 +1,4 @@
-import dj_database_url
-
 from .base import *  # noqa pylint: disable=wildcard-import, unused-wildcard-import
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
