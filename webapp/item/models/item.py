7db3a045708f226a0f2d0ca923c19bcd6ec7c52c
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-12 02:12:46 +0900

    feat: item display_order 추가

--- webapp/item/models/item.py
+++ webapp/item/models/item.py
@@ -1,5 +1,12 @@
-from config.models import BaseModel
+from config.models import BaseModel, ModelManager
 from django.db import models
+
+
+class ItemModelManager(ModelManager):
+    """Model Manager definition for Category"""
+
+    def get_queryset(self):
+        return super().get_queryset().order_by("-display_order")
 
 
 class Item(BaseModel):
@@ -17,13 +24,31 @@ class Item(BaseModel):
         verbose_name="아이템명",
     )
 
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=30, default="map", verbose_name="아이템 종류")
-
-    thumbnail = models.ForeignKey(
-        "file_manager.Image", related_name="items", on_delete=models.CASCADE, verbose_name="아이템 이미지"
+    category = models.CharField(
+        choices=CATEGORY_CHOICES,
+        max_length=30,
+        default="map",
+        verbose_name="아이템 종류",
     )
 
-    point = models.PositiveIntegerField(default=0, verbose_name="아이템 가격")
+    thumbnail = models.ForeignKey(
+        "file_manager.Image",
+        related_name="items",
+        on_delete=models.CASCADE,
+        verbose_name="아이템 이미지",
+    )
+
+    point = models.PositiveIntegerField(
+        default=0,
+        verbose_name="아이템 가격",
+    )
+
+    display_order = models.IntegerField(
+        default=0,
+        verbose_name="표시 순서",
+    )
+
+    objects = ItemModelManager()
 
     class Meta:
         db_table = "items"
