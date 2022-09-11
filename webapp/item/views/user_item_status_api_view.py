a0394aa7ba8772aa87eced44f9fbf61d9ff11d35
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-11 22:49:49 +0900

    refactor: 프론트에서 사용하기 쉬운 형식으로 변경

--- webapp/item/views/user_item_status_api_view.py
+++ webapp/item/views/user_item_status_api_view.py
@@ -2,6 +2,7 @@ from config.views import BaseAPIView
 from django.contrib.auth import get_user_model
 from django.db.models import F, Sum
 from django.shortcuts import get_object_or_404
+from item.models import Item
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -14,11 +15,16 @@ class UserItemStatusAPIView(BaseAPIView):
 
     def get(self, request, user_id, *args, **kwargs):
         user = get_object_or_404(User, pk=user_id)
-        item_status = (
+        ret = dict([[c[0], 0] for c in Item.CATEGORY_CHOICES])
+
+        item_statuses = (
             user.user_items.select_related("item")
             .values("item__category", "count")
             .annotate(total_count=Sum("count"), category=F("item__category"))
             .values("category", "total_count")
         )
 
-        return Response(item_status, status=status.HTTP_200_OK)
+        for item_status in item_statuses:
+            ret[item_status["category"]] = item_status["total_count"]
+
+        return Response(ret, status=status.HTTP_200_OK)
