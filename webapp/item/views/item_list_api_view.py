3e5d092c0e3e8922979ac455368b87269cb63443
shinkeonkim <singun11@kookmin.ac.kr>
2022-09-11 20:50:42 +0900

    fix: serializer context가 날라간 이슈

--- webapp/item/views/item_list_api_view.py
+++ webapp/item/views/item_list_api_view.py
@@ -16,18 +16,24 @@ class ItemListAPIView(ListAPIView):
     def current_user(self):
         return self.request.user
 
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        buyable_item_check_results = BuyableItemCheckService(self.request).list()
+
+        return {
+            "request": self.request,
+            "format": self.format_kwarg,
+            "view": self,
+            "buyable_item_check_results": buyable_item_check_results,
+        }
+
     # 사용자의 조건에 맞는 아이템들만 보여주기
     def get(self, request, *args, **kwargs):
         # 사용자가 갖고 있는 아이템들
-        buyable_item_check_results = BuyableItemCheckService(request).list()
 
         queryset = self.get_queryset().select_related("thumbnail")
-        serializer = self.get_serializer(
-            queryset,
-            many=True,
-            context={
-                "buyable_item_check_results": buyable_item_check_results,
-            },
-        )
+        serializer = self.get_serializer(queryset, many=True, context=self.get_serializer_context())
 
         return Response(serializer.data, status=status.HTTP_200_OK)
