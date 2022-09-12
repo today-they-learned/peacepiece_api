0b06c00a391cfcffed81875d1e8c6fd996144dee
Jihyun-Choi <034179@naver.com>
2022-09-12 21:03:00 +0900

    feat: 포인트 이력 리스트 API 구현

--- webapp/point/urls.py
+++ webapp/point/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from point.views import PointHistoryListAPIView
 
 app_name = "point"
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "my/",
+        PointHistoryListAPIView.as_view(),
+        name="PointHistory",
+    ),
+]
---
+++ webapp/point/views/point_history_list_api_view.py
@@ -0,0 +1,23 @@
+from config.views import BaseAPIView
+from point.models import Point
+from point.serializers.point_serializer import PointSerializer
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
+
+class PointHistoryListAPIView(ListAPIView, BaseAPIView):
+    serializer_class = PointSerializer
+    filterset_fields = []
+    search_fields = []
+    ordering_fields = []
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Point.objects.filter(user=self.current_user).order_by("-updated_at")
+
+    def list(self, request, *args, **kwargs):
+        """
+        포인트 이력 리스트 API
+        : 최신순으로 반환
+        """
+        return super().list(self, request, args, kwargs)
