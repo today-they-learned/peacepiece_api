07f7b50d3014cdce25903835447c7721c73945d7
Jin409 <sualng123@gmail.com>
2022-09-12 14:01:27 +0900

    feat: feedback_count 를 초기화하는 어드민 액션 추가

--- webapp/challenge/admin/challenge_suggestion.py
+++ webapp/challenge/admin/challenge_suggestion.py
@@ -1,11 +1,21 @@
-from challenge.models import ChallengeSuggestion, ChallengeSuggestionFeedback
 from config.admin import linkify
 from django.contrib import admin
+
+from challenge.models import ChallengeSuggestion, ChallengeSuggestionFeedback
 
 
 def reset_counters(self, request, queryset):
     for challenge_suggestion in queryset:
         challenge_suggestion.reset_feedback_count()
+
+
+def reset_feedback_count(self, request, queryset):
+    for challenge_suggestion in queryset:
+        challenge_suggestion.feedback_count = 0
+        challenge_suggestion.save()
+
+
+reset_feedback_count.short_description = "feedback_count 를 초기화합니다."
 
 
 @admin.register(ChallengeSuggestion)
@@ -21,3 +31,4 @@ class ChallengeSuggestionAdmin(admin.ModelAdmin):
     )
     readonly_fields = ("challenge", "suggester")
     ordering = ("suggester", "feedback_count")
+    actions = (reset_feedback_count,)
